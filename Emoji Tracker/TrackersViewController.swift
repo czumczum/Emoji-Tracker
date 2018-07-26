c06b37b9756e1339a38ba2a5c18b5137f5a1ab70
Ula <slodkosci@gmail.com>
2018-07-26 13:53:36 -0400

    Initial Commit

---
+++ Emoji Tracker/TrackersViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TrackersViewController.swift
+//  Emoji Tracker
+//
+//  Created by Ula Kuczynska on 7/26/18.
+//  Copyright © 2018 Ula Kuczynska. All rights reserved.
+//
+
+//import UIKit
+//
+//class ViewController: UIViewController {
+//    
+//
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+//        navigationController?.navigationBar.shadowImage = UIImage()
+//        navigationController?.navigationBar.isTranslucent = true
+//    }
+//
+//    
+//    
+//}
---
+++ Emoji Tracker/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Emoji Tracker
+//
+//  Created by Ula Kuczynska on 7/25/18.
+//  Copyright © 2018 Ula Kuczynska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var daysStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+    }
+
+    @IBAction func addNewScreenButton(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
