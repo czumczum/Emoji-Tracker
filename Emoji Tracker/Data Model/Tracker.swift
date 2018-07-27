012b74250305a7c9e2ac08a776ee669eb5481d91
Ula <slodkosci@gmail.com>
2018-07-27 13:55:33 -0400

    Core Data added, Create new tracker screen

---
+++ Emoji Tracker/Data Model/Tracker.swift
@@ -0,0 +1,16 @@
+//
+//  Tracker.swift
+//  Emoji Tracker
+//
+//  Created by Ula Kuczynska on 7/27/18.
+//  Copyright © 2018 Ula Kuczynska. All rights reserved.
+//
+
+import Foundation
+
+class Trackers {
+    var name : String = ""
+    var emojis : String = ""
+    var category : String = ""
+    var archived : Bool = false
+}
--- Emoji Tracker/ViewController.swift
+++ Emoji Tracker/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var trackers : [Trackers] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.isTranslucent = true
+        addNewTrackerButton?.isHidden = true
     }
 
 
@@ -30,12 +33,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var trackersTableView: UITableView!
     @IBOutlet weak var addNewTrackerButtonClicked: UIBarButtonItem!
     
-    // MARK: - Adding new tracker Swipping pages
+    //MARK: - Stats Swipping pages
     
-    @IBAction func pagination(_ sender: UIPageControl) {
+    @IBAction func dismissStatsButtonClicked(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
+    //MARK: - Create new tracker
+    
 
+    @IBAction func dismissNewItemButtonClicked(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func addNewTrackerButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBOutlet var addNewTrackerButton: UIButton!
+    @IBOutlet var newTitleLabel: UILabel!
+    @IBOutlet var newEmojisLabel: UILabel!
+    
+    @IBOutlet var newTrackerName: UITextField!
+    @IBAction func newTrackerTypeInputClicked(_ sender: UIButton) {
+    }
+    @IBAction func newTrackerTypePick5Clicked(_ sender: UIButton) {
+    }
+    @IBAction func newTrackerTypeSliderClicked(_ sender: UIButton) {
+    }
+    @IBOutlet var newTrackerEmojis: UITextField!
+    
+    
+    
     
 
 }
